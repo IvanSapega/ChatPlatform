17d660cfc38a071288299a27e2ed686e0c4a9239
IvanSapega <34104112+IvanSapega@users.noreply.github.com>
2018-06-04 20:51:19 +0300

    ugly design

--- Garbage/TelePound/script.js
+++ Garbage/TelePound/script.js
@@ -1,7 +1,10 @@
 var uname = document.getElementsByClassName("username");
 var ddmenu = document.getElementsByClassName("dropdown");
 var tds = document.getElementsByClassName("tbl");
-var ddactive = false;
+var ddactive = false,
+    createActive = false;
+var create = document.getElementById("create");
+var createO = document.getElementsByClassName("create")[0];
 uname[0].onclick = function() {
     //console.log(ddactive);
     if (ddactive) {
@@ -11,7 +14,20 @@ uname[0].onclick = function() {
 
     } else {
         tds[0].style.display = "block";
-        ddmenu[0].style.height = "50px";
+        ddmenu[0].style.height = "75px";
         ddactive = true;
+    }
+}
+create.onclick = function() {
+    console.log("heh");
+    if (createActive) {
+        createO.style.display = "none";
+        createO.style.height = "0px";
+        createActive = false;
+
+    } else {
+        createO.style.display = "block";
+        createO.style.height = "200px";
+        createActive = true;
     }
 }
